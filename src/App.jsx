2c5ccb3f1efb5082c196ba9b37d55e63a2a0dcdd
Miguel Gomez <miguelgomeze17@gmail.com>
2024-10-22 18:31:46 -0500

    create routes and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./pages/Shop/Shop";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element ={<Shop}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
