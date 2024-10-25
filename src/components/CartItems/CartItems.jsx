ebfbcf9d55e250a963230a7c1f2eaed186ca631e
Miguel Gomez <miguelgomeze17@gmail.com>
2024-10-25 16:47:57 -0500

    add styling for cart component

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./CartItems.scss";
+import { ShopContext } from "../../context/ShopContext";
+import remove_icon from "../../assets/cart_cross_icon.png";
 
 function CartItems() {
-  return <div>CartItems</div>;
+  const { all_product, cartItem, removeFromCart } = useContext(ShopContext); // Fixed cartItems to cartItem
+
+  return (
+    <div className="cart-items">
+      <div className="cart-items__format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {all_product.map((e) => {
+        if (cartItem[e.id] > 0) {
+          // Use cartItem here
+          return (
+            <div key={e.id}>
+              {" "}
+              {/* Added key for React */}
+              <div className="cart-items__format cart-items__format-main">
+                <img
+                  className="cart-items__product-icon"
+                  src={e.image}
+                  alt=""
+                />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cart-items__quantity">
+                  {cartItem[e.id]} {/* Corrected access to cartItem */}
+                </button>
+                <p>${e.new_price * cartItem[e.id]}</p>{" "}
+                {/* Fixed total price calculation */}
+                <img
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  className="cart-items__remove-icon"
+                  src={remove_icon}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+        return null; // Ensure map always returns something
+      })}
+    </div>
+  );
 }
 
 export default CartItems;
